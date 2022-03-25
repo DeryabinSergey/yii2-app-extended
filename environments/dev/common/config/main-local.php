ff7c2880564b79ea2c30b240ddbe50762ad4d661
Deriabin Sergei <deryabinsergey@gmail.com>
2022-03-25 10:23:20 +0100

    Change deprecated Swiftmailer to Symfonymailer

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -3,14 +3,14 @@
 return [
     'components' => [
         'db' => [
-            'class' => 'yii\db\Connection',
+            'class' => \yii\db\Connection::class,
             'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
             'username' => 'root',
             'password' => '',
             'charset' => 'utf8mb4',
         ],
         'mailer' => [
-            'class' => 'yii\swiftmailer\Mailer',
+            'class' => \yii\symfonymailer\Mailer::class,
             'viewPath' => '@common/mail',
             // send all mails to a file by default. You have to set
             // 'useFileTransport' to false and configure a transport
